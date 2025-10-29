ac87ffd602b64ea934da7fe689c12e66fe9d9f3a
Peter Parker <petrfalkovsky@yandex.ru>
2025-10-29 16:41:30 +0000

    init: initial commit

---
+++ Productab_test/Productab_test/Productab_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  Productab_testApp.swift
+//  Productab_test
+//
+//  Created by poteryal_trusy on 29.10.2025.
+//
+
+import SwiftUI
+
+@main
+struct Productab_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
